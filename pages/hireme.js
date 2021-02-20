4b3d7eaddd37a015af41bad1da01035c97cc2d6a
deepak bhatt <deepakbhattmits@gmail.com>
2021-02-20 09:06:00 +0530

    Update hireme.js

--- pages/hireme.js
+++ pages/hireme.js
@@ -2,7 +2,7 @@
 import Layout from '../components/Layout';
 const hireme = () => (
 	<Layout title='Hire Me'>
-		You can Hire me at
+		<h1>You can Hire me at</h1>
 		<a href='mailto:deepakbhattmits@gmail.com'>deepakbhattmits@gmail.com</a>
 	</Layout>
 );
