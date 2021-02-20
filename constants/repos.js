63abbdf4d4f3e299d48cc30081e5d791dff33236
deepak bhatt <deepakbhattmits@gmail.com>
2021-02-20 09:05:07 +0530

    Create repos.js

---
+++ constants/repos.js
@@ -0,0 +1,67 @@
+/** @format */
+
+export const repos = [
+	{
+		appName: 'Favourite Bar ',
+		repoUrl: 'https://github.com/deepakbhattmits/app-proj-bar',
+		webUrl: 'https://app-proj-bar.herokuapp.com/',
+		description:
+			'this is a app where you can add and remove beers favorite (like wishlist)',
+		stacks: [
+			'react js',
+			'redux',
+			'semantic UI',
+			'react-toastify',
+			'node-sass',
+			'redux-persist',
+			'redux-thunk',
+			'node-sass',
+			'enzyme',
+		],
+	},
+	{
+		appName: 'Ecommerce App',
+		repoUrl: 'https://github.com/deepakbhattmits/ecom-turing',
+		webUrl: 'https://ecom-turing.herokuapp.com/',
+		description: 'this is a ecommerce app',
+		stacks: [
+			'react js',
+			'redux',
+			'semantic UI',
+			'axios',
+			'node-sass',
+			'react-alice-carousel',
+			'redux-persist',
+			'redux-thunk',
+		],
+	},
+	{
+		appName: 'Next cookie authentication',
+		repoUrl: 'https://github.com/deepakbhattmits/next-cookie-auth',
+		webUrl: 'https://next-cookie-auth.deepakbhattmits.vercel.app',
+		description: 'this is built in nextjs, cooki based authentication',
+		stacks: ['nextjs', 'react', 'react-dom'],
+	},
+	{
+		appName: 'App Places',
+		repoUrl: '',
+		webUrl: 'https://app-places-2d069.web.app/',
+		description: 'user can add, update and delete places used mongoDB database',
+		stacks: ['react js', 'redux', 'mongodb', 'node js'],
+	},
+	{
+		appName: 'node js shop',
+		repoUrl: 'https://github.com/deepakbhattmits/app-node',
+		webUrl: 'https://app-node-home.herokuapp.com/',
+		description:
+			'a shop build in node js and use view engine EJS useing database mongoDB',
+		stacks: [
+			'react js',
+			'redux',
+			'semantic UI',
+			'mongoDB',
+			'view engine(EJS)',
+			'nodejs',
+		],
+	},
+];
