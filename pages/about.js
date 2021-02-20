047a654dae87c7eff8361716ab4deee557ddaab5
deepak bhatt <deepakbhattmits@gmail.com>
2021-02-20 09:05:39 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -16,7 +16,21 @@ const About = ({ user = {}, statusCode }) => {
 	return (
 		<Layout title='About'>
 			<img src={user.avatar_url} alt={user.name} height='100' width='100' />
-			<p>{user.name}</p>
+			{/* <p>{user.name}</p> */}
+			<p className='description'>
+				So I'm Deepak Bhatt, I am living in India. Hobbyist in general, I'm just
+				always curious. I'd like to think I'm a solid web developer, but I know
+				I have much to learn, and I make it a mission to learn something new
+				every day. I hope I get the chance to grow withyou!
+			</p>
+			<style jsx>{`
+				img {
+					margin-top: 10px;
+				}
+				.description {
+					width: 80%;
+				}
+			`}</style>
 		</Layout>
 	);
 };
