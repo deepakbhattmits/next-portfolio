48582792c5bc18a1bfb4cff0f57a7241c92dec3e
deepak bhatt <deepakbhattmits@gmail.com>
2021-02-20 09:07:22 +0530

    Create Main.jsx

---
+++ components/Main.jsx
@@ -0,0 +1,12 @@
+/** @format */
+import Card from './Card';
+import { repos } from '../constants';
+
+const Main = () => {
+	return (
+		<>
+			<Card repos={repos} />
+		</>
+	);
+};
+export default Main;
