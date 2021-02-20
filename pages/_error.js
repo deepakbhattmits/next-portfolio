6cec22e47ddf18e704b2ea46940d840e19149eba
deepak bhatt <deepakbhattmits@gmail.com>
2021-02-20 09:05:47 +0530

    Update _error.js

--- pages/_error.js
+++ pages/_error.js
@@ -1,14 +1,16 @@
 /** @format */
 
 import Layout from '../components/Layout';
+import Custom404 from './404';
 
 const Error = ({ statusCode, message }) => {
 	return (
-		<Layout title='Error!!'>
-			{statusCode
-				? `Could not load user data : Status code ${statusCode}`
-				: `Couldn't get that page, Sorry!`}
-		</Layout>
+		// <Layout title='Error!!'>
+		// 	{statusCode
+		// 		? `Could not load user data : Status code ${statusCode}`
+		// 		: `Couldn't get that page, Sorry!`}
+		// </Layout>
+		<Custom404 />
 	);
 };
 export default Error;
