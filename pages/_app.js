34692366d6d1693ca21089b3f6671206f4c964dd
deepak bhatt <deepakbhattmits@gmail.com>
2021-02-20 09:05:43 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 /** @format */
 
 import Head from 'next/head';
+import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => {
 	return (
 		<>
 			<Head>
 				<meta name='viewport' content='viewport-fit=cover' />
+				<title>App Portfolio</title>
 			</Head>
 			<Component {...pageProps} />
 		</>
