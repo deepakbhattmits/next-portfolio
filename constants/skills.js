1f64b679afc41952aba9aa9b5990e499df44cc23
deepak bhatt <deepakbhattmits@gmail.com>
2021-02-20 09:05:03 +0530

    Create skills.js

---
+++ constants/skills.js
@@ -0,0 +1,21 @@
+/** @format */
+
+// export const skills = [
+// 	'react',
+// 	'react-native',
+// 	'node',
+// 	'deno',
+// 	'graphQl',
+// 	'MongoDB',
+// 	"css preprocessor's",
+// ];
+export const skills = [
+	{ name: 'react', logo: 'react' },
+	{ name: 'react-native', logo: 'react' },
+	{ name: 'javascript', logo: 'javascript' },
+	{ name: 'node', logo: 'nodejs' },
+	{ name: 'deno', logo: 'deno' },
+	{ name: 'graphQl', logo: 'graphql' },
+	{ name: 'MongoDB', logo: 'mongodb' },
+	{ name: "css preprocessor's", logo: 'sass' },
+];
