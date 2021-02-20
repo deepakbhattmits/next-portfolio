3c22ae9d0edf11490b873a69e7094288b27f6a5f
deepak bhatt <deepakbhattmits@gmail.com>
2021-02-20 09:05:22 +0530

    Create 404.js

---
+++ pages/404.js
@@ -0,0 +1,16 @@
+/** @format */
+
+const Custom404 = () => {
+	return (
+		<>
+			<h1>404 - Page Not Found</h1>
+			<style jsx>{`
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				height: calc(100vh);
+			`}</style>
+		</>
+	);
+};
+export default Custom404;
