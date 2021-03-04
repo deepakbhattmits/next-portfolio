45db43b253d2e1824cfd690a919b765e9fec582e
deepak bhatt <deepakbhattmits@gmail.com>
2021-03-04 10:41:06 +0530

    Delete hireme.js

--- pages/hireme.js
+++
@@ -1,9 +0,0 @@
-/** @format */
-import Layout from '../components/Layout';
-const hireme = () => (
-	<Layout title='Hire Me'>
-		<h1>You can Hire me at</h1>
-		<a href='mailto:deepakbhattmits@gmail.com'>deepakbhattmits@gmail.com</a>
-	</Layout>
-);
-export default hireme;
