77498f82e57fa1dcd943350ec3eeeb27a2529c8b
deepak bhatt <deepakbhattmits@gmail.com>
2021-05-07 08:28:08 +0530

    Create useLogin.js

---
+++ hooks/useLogin.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import { axios } from 'axios';
+const useLogin = () => {
+	const loginService = (url, data) => {
+		return axios.post(url, {
+			method: 'POST',
+			body: data,
+		});
+	};
+
+	return { loginService };
+};
